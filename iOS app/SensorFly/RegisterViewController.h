38507b8f884cffe998b3c82bb1851fc9ca27b1f8
Carlos Ruiz <carlos.ruiz.dominguez@west.cmu.edu>
2015-03-11 23:23:45 -0700

    Updated iOS code with phone refreshing rotation and displacement

---
+++ iOS app/SensorFly/RegisterViewController.h
@@ -0,0 +1,14 @@
+//
+//  GroundTruthViewController.h
+//  SensorFly
+//
+//  Created by Juan Sebastian on 2/16/15.
+//  Copyright (c) 2015 Juan Sebastian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+- (IBAction)tappedSend:(id)sender;
+@property (strong, nonatomic) IBOutlet UITextField *inputField;
+@end
